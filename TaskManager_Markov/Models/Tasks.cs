786fe26bd090272dd8af680701e0f3d7a80d3530
nino-allure <kxzuy@vk.com>
2026-05-03 14:42:59 +0500

    Разработанная модель задач.  markov

---
+++ TaskManager_Markov/Models/Tasks.cs
@@ -0,0 +1,162 @@
+﻿using TaskManager_Markov.Classes;
+using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Schema = System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager_Markov.Models
+{
+    public class Tasks : Notification
+    {
+        public int Id { get; set; }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                Match match = Regex.Match(value, "^.{1,50}$");
+                if (!match.Success)
+                    MessageBox.Show("Накосячил на вводе - ЛОХ! (а еще не более 50 символов)");
+                else
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        private string priority;
+        public string Priority
+        {
+            get { return priority; }
+            set
+            {
+                Match match = Regex.Match(value, "^.{1,30}$");
+                if (!match.Success)
+                    MessageBox.Show("Накосячил на вводе - ЛОХ! (а еще не более 30 символов)");
+                else
+                {
+                    priority = value;
+                    OnPropertyChanged("Priority");
+                }
+            }
+        }
+
+        private DateTime dateExecute;
+        public DateTime DateExecute
+        {
+            get { return dateExecute; }
+            set
+            {
+                if (value.Date < DateTime.Now.Date)
+                    MessageBox.Show("А те норм что дата меньше текущей?");
+                else
+                {
+                    dateExecute = value;
+                    OnPropertyChanged("DateExecute");
+                }
+            }
+        }
+
+        private string comment;
+        public string Comment
+        {
+            get { return comment; }
+            set
+            {
+                Match match = Regex.Match(value, "^.{1,1000}$");
+                if (!match.Success)
+                    MessageBox.Show("Накосячил на вводе - ЛОХ! (1000 символов строчи)");
+                else
+                {
+                    comment = value;
+                    OnPropertyChanged("Comment");
+                }
+            }
+        }
+
+        public bool done;
+        public string Done
+        {
+            get { return done; }
+            set
+            {
+                done = value;
+                OnPropertyChanged("Done");
+                OnPropertyChanged("IsDoneText");
+                
+            }
+        }
+
+        [Schema.NotMapped]
+        private bool isEnable;
+        [Schema.NotMapped]
+        public bool IsEnable
+        {
+            get { return isEnable; }
+            set
+            {
+                isEnable = value;
+                OnPropertyChanged("IsEnable");
+                OnPropertyChanged("IsEnableText");
+            }
+        }
+        [Schema.NotMapped]
+        public string IsEnableText
+        {
+            get
+            {
+                if (IsEnable) return "Сохранить";
+                else return "Изменить";
+            }
+        }
+        [Schema.NotMapped]
+        public string IsDoneText
+        {
+            get
+            {
+                if (Done) return "Не выполнено";
+                else return "Выполенено";
+            }
+        }
+        [Schema.NotMapped]
+        public RelayCommand OnEdit
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    IsEnable = !IsEnable;
+                    if (!IsEnable)
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.SaveChanges();
+                });
+            }
+        }
+        [Schema.NotMapped]
+        public RelayCommand OnDelete
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    if (MessageBox.Show("Мы это удаляем?", "БАЗАР", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.Tasks.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_tasks.tasksContext.SaveChanges();
+                    }
+                });
+            }
+        }
+        [Schema.NotMapped]
+        public RelayCommand OnDone
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    Done = !Done;
+                });
+        }
+    }
+}
