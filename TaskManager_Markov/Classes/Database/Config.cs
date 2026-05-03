acbb26f1ebcb7bd06b11eb8a52b9e949b1e16eee
nino-allure <kxzuy@vk.com>
2026-05-03 14:12:05 +0500

    Созданный класс Config.  Перенос кода с флешки. Марков

---
+++ TaskManager_Markov/Classes/Database/Config.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_Markov.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=localhost;" +
+            "uid=root;" + "pwd=;" + "database=TaskManager;";
+
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
